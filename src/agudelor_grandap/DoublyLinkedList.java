3c77d985a33734bc8558fd865cfdb9aea7f4676c
SmithAR <audoban@github.com>
2016-03-10 15:15:08 -0500

    added initial DoublyLinkedList

---
+++ src/agudelor_grandap/DoublyLinkedList.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agudelor_grandap;
+
+/**
+ *
+ * @author audoban
+ */
+public class DoublyLinkedList implements List<E> {
+    
+    private DoublyLinkedNode<E> front;
+
+    @Override
+    public void add(E target) {
+    }
+
+    @Override
+    public boolean contains(E target) {
+    }
+
+    @Override
+    public E get(int index) {
+    }
+
+    @Override
+    public boolean isEmpty() {
+    }
+
+    @Override
+    public E remove(int index) {
+    }
+
+    @Override
+    public boolean remove(E target) {
+    }
+
+    @Override
+    public void set(int index, E target) {
+    }
+
+    @Override
+    public int size() {
+    }
+    
+    
+}
